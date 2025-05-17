102cb695a6b2ee4f4a93b4aaa68b97fee51de96f
sang.lee <sangle.qc.98@gmail.com>
2025-05-17 23:19:03 +0700

    Setup Appium POM framework with BasePage,BaseTest, ConfigReader, and implement Fill Form test case

---
+++ src/main/java/commonPage/BasePage.java
@@ -0,0 +1,16 @@
+package common;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+
+public class BasePage {
+    protected AndroidDriver driver;
+
+    public BasePage(AndroidDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
+    }
+}
